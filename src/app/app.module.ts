4d317ffb77a7477b5e7531ff1e8a603cc8f270f3
Rares Catalin Onescu <raresonescu@gmail.com>
2021-08-07 22:25:07 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import{PostCreateComponent} from './posts/post-create/post-create.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
