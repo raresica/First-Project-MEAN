4ececc48a34e5666dc19534d9e3a19b6a779dca7
Rares Catalin Onescu <raresonescu@gmail.com>
2021-08-08 23:14:58 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import{PostCreateComponent} from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule} from '@angular/material/input';
+import{MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,8 +17,15 @@ import{PostCreateComponent} from './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,12 +3,15 @@ import { Component } from "@angular/core";
 @Component({
 selector: 'app-post-create',
 templateUrl: './post-create.component.html',
+styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent{
-newPost ='';
+enteredValue="";
+newPost ='NO CONTENT';
 
   onAddPost() {
-    this.newPost='The user\'s post';
+
+    this.newPost=this.enteredValue;
   }
 
 }
